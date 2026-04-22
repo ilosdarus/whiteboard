0bea4ce359756614b7577e50bca3ee3ab27eeb88
ilosdarus <mcmahongary540@outlook.com>
2026-04-22 18:21:08 +0800

    init

--- scripts/server.js
+++ scripts/server.js
@@ -22,12 +22,12 @@ const server_mode = args.mode === "production" ? SERVER_MODES.PRODUCTION : SERVE
 if (server_mode === SERVER_MODES.DEVELOPMENT) {
     let startFrontendDevServer = (await import("./server-frontend-dev.js")).startFrontendDevServer;
     console.info("Starting server in development mode.");
-    startFrontendDevServer(8080, function () {
+    startFrontendDevServer(8088, function () {
         // this time, it's the frontend server that is on port 8080
         // requests for the backend will be proxied to prevent cross origins errors
-        startBackendServer(3000);
+        startBackendServer(3011);
     });
 } else {
     console.info("Starting server in production mode.");
-    startBackendServer(process.env.PORT || 8080);
+    startBackendServer(process.env.PORT || 8088);
 }
